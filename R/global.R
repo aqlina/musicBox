86c39cdea7d18ae4b049530e73063130f26520ab
aqlina <tselinina@gmail.com>
2020-10-25 09:41:50 -0400

    add documentation about package itself

--- R/global.R
+++ R/global.R
@@ -2,14 +2,14 @@
 #'
 #' Launch MusicBox Shiny App
 #'
-#' This function launches MusicBox Shiny App based on the data from Postgres database 
+#' This function launches MusicBox Shiny App based on the data from Postgres database
 #' which must be connected to R before launching.
-#' 
+#'
 #' @export
 #' @author "Alina Tselinina <tselinina@gmail.com>"
 #'
 #' @import shiny
-#' 
+#'
 #' @aliases \link{musicBox_ui} \link{musicBox_server}
 
 launch_app <- function(){
